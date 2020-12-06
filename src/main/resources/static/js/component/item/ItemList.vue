35564fc62130c173599be1120fa7f739fd64d045
Danil <dlyaregistr123@bk.ru>
2020-12-06 13:14:41 +0700

    commit before new branch

--- src/main/resources/static/js/component/item/ItemList.vue
+++ src/main/resources/static/js/component/item/ItemList.vue
@@ -1,11 +1,6 @@
 <template>
     <v-container>
-
         <item-row v-for="item in items" :item="item" :key="item.id">
-            <template v-slot:text>LOL</template>
-            <template v-slot:btn>
-                <v-btn>kek</v-btn>
-            </template>
         </item-row>
     </v-container>
 </template>
--- src/main/resources/static/js/component/item/ItemRow.vue
+++ src/main/resources/static/js/component/item/ItemRow.vue
@@ -3,11 +3,9 @@
         <v-card-text primary-title>
             {{ item.icon }}
             {{ item.name }}
-            <slot name="text"/>
         </v-card-text>
         <v-card-actions>
             <v-btn :to="`/item/${item.id}`">open</v-btn>
-            <slot name="btn"/>
         </v-card-actions>
     </v-card>
 </template>
