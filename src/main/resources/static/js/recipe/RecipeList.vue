613f988559d3d89febbf9691498367783373b1e8
Danil <dlyaregistr123@bk.ru>
2020-12-08 12:23:18 +0700

    added recipe-panels-tree

--- src/main/resources/static/js/recipe/RecipeList.vue
+++ src/main/resources/static/js/recipe/RecipeList.vue
@@ -4,21 +4,19 @@
             <h1>This item has no craft recipe</h1>
         </div>
         <div v-else>
-            <h1>Here's craft recipe</h1>
+            <h1>Here's craft recipe of "{{item.name}}"</h1>
             <div>
-                <template>
-                    <recipe-row v-for="entry in item.craftRecipe" :entry="entry" :key="entry.item.id"></recipe-row>
-                </template>
+                <recipe-tree :item="item" :amount="1"></recipe-tree>
             </div>
         </div>
     </v-container>
 </template>
 <script>
     import ItemRow from "../component/item/ItemRow";
-    import RecipeRow from "./RecipeRow";
+    import RecipeTree from "./RecipePanelsTree";
 
     export default {
         props: ["item"],
-        components: {RecipeRow, ItemRow},
+        components: {RecipeTree, ItemRow},
     }
 </script>
---
+++ src/main/resources/static/js/recipe/RecipePanelsTree.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-expansion-panels multiple>
+        <v-expansion-panel v-if="item" v-for="entry in item.craftRecipe" :entry="entry" :key="entry.item.id">
+            <v-expansion-panel-header>{{entry.item.name}}</v-expansion-panel-header>
+            <v-expansion-panel-content :set="resultAmount = entry.amount * amount">
+                {{resultAmount}}
+                <recipe-tree :item="entry.item" :amount="resultAmount"></recipe-tree>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
+
+<script>
+    export default {
+        name: "recipeTree",
+        props: ['item', 'amount']
+    }
+</script>
